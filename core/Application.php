9119185b9de1321ff4efae36f84c70a3d1821bb3
周子凱 <andychou@zhouzikaideMacBook-Air.local>
2020-10-24 22:05:28 +0800

    feat: 建立PHP框架(Views)

--- core/Application.php
+++ core/Application.php
@@ -2,22 +2,29 @@
 
 namespace app\core;
 
-use app\core\Request;
 use app\core\Router;
+use app\core\Request;
+use app\core\Response;
 
 class Application
 {
-    public $router;
+    public static $ROOT_DIR;
+    public static $app;
     public $request;
+    public $response;
+    public $router;
 
-    public function __construct()
+    public function __construct($rootPath)
     {
+        self::$ROOT_DIR = $rootPath;
+        self::$app = $this;
         $this->request = new Request();
-        $this->router = new Router($this->request);
+        $this->response = new Response();
+        $this->router = new Router($this->request, $this->response);
     }
 
     public function run()
     {
-        $this->router->resolve();
+        echo $this->router->resolve();
     }
 }
--- core/Router.php
+++ core/Router.php
@@ -7,10 +7,13 @@ use app\core\Request;
 class Router
 {
     protected $routes = [];
+    public $request;
+    public $response;
 
-    public function __construct(Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
     public function get($path, $callback)
@@ -23,12 +26,37 @@ class Router
         $path = $this->request->getPath();
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
-
+        
         if ($callback === false) {
-            echo 'NOT FOUND';
-            exit;
+            $this->response->setStatusCode(404);
+            return 'NOT FOUND';
+        }
+
+        if (is_string($callback)) {
+            return $this->renderView($callback);
         }
         
-        echo call_user_func($callback);
+        return call_user_func($callback);
+    }
+
+    public function renderView($view)
+    {
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+    }
+
+    protected function layoutContent()
+    {
+        ob_start(); // start cache the output
+        include_once Application::$ROOT_DIR . "/views/layouts/main.php";
+        return ob_get_clean(); // get cache and clear
+    }
+
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/$view.php";
+        return ob_get_clean();
     }
 }
