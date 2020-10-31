dbfb13fd0a142e17b1bb409ce1d4a4870ce308dc
周子凱 <andychou@zhouzikaideMacBook-Air.local>
2020-11-01 01:37:34 +0800

    feat: 建立PHP框架(實作migrations)

--- core/Database.php
+++ core/Database.php
@@ -15,4 +15,51 @@ class Database
         $this->pdo = new \PDO($dsn, $user, $password);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
+
+    public function applyMigrations()
+    {
+        $this->createMigrationsTable();
+        $appliedMigrations = $this->getAppliedMigrations();
+
+        $files = scandir(Application::$ROOT_DIR . '/migrations');
+        $toApplyMigrations = array_diff($files, $appliedMigrations);
+
+        $newMigrations = [];
+        foreach($toApplyMigrations as $migration) {
+            if ($migration === '.' || $migration === '..') {
+                continue;
+            }
+
+            require_once Application::$ROOT_DIR . '/migrations/' . $migration;
+            $className = pathinfo($migration, PATHINFO_FILENAME);
+            $instance = new $className();
+            echo "Applying migration $migration" . PHP_EOL;
+            $instance->up();
+            echo "Applied migration $migration" . PHP_EOL;
+            $newMigrations[] = $migration;
+        }
+
+        if (!empty($newMigrations)) {
+            $this->saveMigrations($newMigrations);
+        } else {
+            echo "All migrations are applied.";
+        }
+    }
+
+    public function createMigrationsTable()
+    {
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS migrations (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            migration VARCHAR(255),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ) ENGINE=INNODB;");
+    }
+
+    public function getAppliedMigrations()
+    {
+        $statement = $this->pdo->prepare("SELECT migration FROM migrations");
+        $statement->execute();
+
+        return $statement->fetchAll(\PDO::FETCH_COLUMN);
+    }
 }
---
+++ migrations/m0001_initial.php
@@ -0,0 +1,14 @@
+<?php
+
+class m0001_initial 
+{
+    public function up()
+    {
+        echo "Apply migrations" . PHP_EOL;
+    }
+
+    public function down()
+    {
+        echo "Down migrations" . PHP_EOL;
+    }
+}
